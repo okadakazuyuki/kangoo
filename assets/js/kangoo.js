f6f61b23f51b6cc9eb4df7a4a41e863d621619d4
岡田和亨 <okadathethird@gmail.com>
2019-08-03 04:32:45 +0900

    修正

--- assets/js/kangoo.js
+++ assets/js/kangoo.js
@@ -4,30 +4,57 @@ var kangoo = function(option){
     throw new Error(mes);
   };
 
-  var render = function(){
+  var flg_change = false; 
+
+  var render = function(next_num){
+    if(stop_flg) return;
     renderer.render( scene, camera );
-    var now_time = (Date.now() - start_time)/1000;
+    
+    var now_time = count/60;
+    var is_change_time = now_time > ( _scene.current + 1 ) * delay + ( _scene.current + 1 ) * dulation;
+
     if(_scene.current < images.length-1){
-      if( now_time > ( _scene.current + 1 ) * delay + ( _scene.current + 1 ) * dulation && _scene.change_finished ) {
+      if(flg_change && _scene.change_finished){
+        _scene.change_finished = false;
+        _scene.changing = true;
+        var next = next_num >= 0 ? next_num : _scene.current + 1;
+        change(textures[_scene.current],textures[next],next);
+      }else if( auto_slide && (is_change_time && _scene.change_finished) ) {
         _scene.change_finished = false;
         _scene.changing = true;
         change(textures[_scene.current],textures[_scene.current + 1]);
       }
     }else{
+      if(auto_slide && !loop && callbacks.ended && typeof callbacks.ended === "function" ) {
+        callbacks.ended();
+        callbacks.ended = false;
+      }
       if(loop){
         if(_scene.current == images.length-1){
-          if( now_time > ( _scene.current + 1 ) * delay + ( _scene.current + 1 ) * dulation && _scene.change_finished ) {
+          if(  (flg_change && _scene.change_finished) || (is_change_time && _scene.change_finished) ) {
             _scene.change_finished = false;
             _scene.changing = true;
             change(textures[_scene.current],textures[0]);
             }
         }else{
-          start_time = Date.now();
           _scene.current = 0;
+          count = 0;
         }
+      }else if(flg_change && _scene.change_finished){
+        _scene.change_finished = false;
+        _scene.changing = true;
+        var next = next_num >= 0 ? next_num : _scene.current + 1;
+        change(textures[_scene.current],textures[next],next);
       }
     }
-    requestAnimationFrame(render);
+    console.log(auto_play,_scene,material);
+    if(!_scene.change_finished){
+      flg_change = false;
+    }
+    count++;
+    if(auto_slide || !_scene.change_finished){
+      requestAnimationFrame(render);
+    }
   };
 
 
@@ -36,8 +63,9 @@ var kangoo = function(option){
     changing: false,
     change_finished: true
   };
-  var count = 0;
-  var change =function(now_tex,next_tex){
+  var stop_flg = false;
+  var count = 1;
+  var change =function(now_tex,next_tex,num){
     mesh.material.uniforms.changingFlg.value = true;
     mesh.material.uniforms.uTex.value = now_tex;
     mesh.material.uniforms.uTex2.value = next_tex;
@@ -47,7 +75,8 @@ var kangoo = function(option){
       onComplete: function(){
         mesh.material.uniforms.uTex.value = next_tex;
         mesh.material.uniforms.changing.value = 0.0;
-        _scene.current++;
+        var next = num >= 0 ? num : _scene.current + 1;
+        _scene.current = next;
         _scene.changing = false;
         _scene.change_finished = true;
       }
@@ -58,14 +87,20 @@ var kangoo = function(option){
 
   var parent = option.parent || error("no selected dom obj"); // need dom obj
   var images = option.images || error("no selected images");// need slide images
-  var loop = option.images || true;// need slide images
+  var mix_file = option.mix_file || error("no selected mix_file");//need dist image name
+  var loop = option.loop === false? false :  true;// need slide images
   var dulation = option.dulation || 0.5; // set transform dulation at seconds
   var delay = option.delay || 5;// set each slide delay at seconds
-  var mode = option.mode || 1;//set by dist image name
-  var autoPlay = option.autoPlay || false;
-  var start_time = Date.now();
+  var direction = option.direction || "default";
+  var auto_slide = option.auto_slide === false ? false :  true;
+  var auto_play = option.auto_play === false ? false :  true;
+  var callbacks = option.callbacks || {loaded:"",ended:""};
 
-  var renderer = new THREE.WebGLRenderer({alpha:true, antialias: true });
+  var renderer = new THREE.WebGLRenderer({
+    alpha:true, antialias: true
+  });
+
+
   renderer.setSize(parent.clientWidth, parent.clientHeight); // レンダラーのサイズをdivのサイズに設定
   renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
   renderer.setClearColor(0x000000, 1.0); // レンダラーの背景色を黒色（不透過）に設定
@@ -91,10 +126,11 @@ var kangoo = function(option){
   var changing = 0.0;
   var canvas = parent;
   var textures = [];
-  var disp = loader.load('assets/images/dist/'+mode+'.jpg');
+  var disp = loader.load(mix_file);
 
   var texture = loader.load(option.images[0],function(){
     texture.minFilter = THREE.LinearFilter;
+    if(callbacks.loaded && typeof callbacks.loaded === "function" ) callbacks.loaded();
 
     option.images.forEach(function(src){
       textures.push(loader.load(src));
@@ -130,7 +166,7 @@ var kangoo = function(option){
     mesh.position.z = 0;
     scene = new THREE.Scene();
     scene.add( mesh );
-    obj = {"mesh": mesh};
+    //obj = {"mesh": mesh};
     
 
     window.addEventListener("resize", function() {
@@ -156,11 +192,18 @@ var kangoo = function(option){
     },false);
 
     window.dispatchEvent(new Event("resize"));
-    render();
+    if(auto_play){
+      render();
+    }
     
 });//End  callBack texture loaded
 
-
+  var directions = {
+    "default": '       vec2 dispP = vec2(vUv.x + changing*disp.r,vUv.y);'+'       vec2 dispP2 = vec2(vUv.x - (1.0 - changing) * disp.r , vUv.y);',
+    "up": '       vec2 dispP = vec2(vUv.x,vUv.y - changing*disp.r);'+'       vec2 dispP2 = vec2(vUv.x , vUv.y + (1.0 - changing) * disp.r);',
+    "down": '       vec2 dispP = vec2(vUv.x,vUv.y + changing*disp.r);'+'       vec2 dispP2 = vec2(vUv.x , vUv.y - (1.0 - changing) * disp.r);',
+    "left": '       vec2 dispP = vec2(vUv.x - changing*disp.r,vUv.y);'+'       vec2 dispP2 = vec2(vUv.x + (1.0 - changing) * disp.r , vUv.y);'
+  };
   var vertex = '   varying vec2 vUv;'+
 '   void main(){'+
 '     vUv = uv;'+
@@ -179,9 +222,7 @@ var kangoo = function(option){
 '       vec4 smpColor0 = texture2D(uTex,vUv);'+
 '       gl_FragColor = smpColor0;'+
 '     }else{'+
-'       vec4 disp =  texture2D(disp, vUv);'+
-'       vec2 dispP = vec2(vUv.x + changing*disp.r,vUv.y);'+
-'       vec2 dispP2 = vec2(vUv.x - (1.0 - changing) * disp.r , vUv.y);'+
+'       vec4 disp =  texture2D(disp, vUv);'+directions[direction]+
 
 '       vec4 smpColor0 = texture2D(uTex,dispP);'+
 '       vec4 smpColor1 = texture2D(uTex2,dispP2);'+
@@ -195,6 +236,22 @@ var kangoo = function(option){
     var new_disp = loader.load('assets/images/dist/'+name+'.jpg');
     mesh.material.uniforms.disp.value = new_disp;
     return true;
+  };
+
+  this.stop = function(){
+    stop_flg = true;
+  };
+  this.auto_slide = auto_slide;
+
+  this.trigger = function(num){
+    if(flg_change || this.auto_slide || !_scene.change_finished) return;
+    console.log("fired",this.auto_slide);
+    flg_change = true;
+    render(num);
+  };
+
+  this.start = function(){
+    render();
   };
   
   return this;
